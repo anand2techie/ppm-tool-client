c4fdd32247376285a8faa44d69ca6f169a6573f8
anand2techie <ananthkec@gmail.com>
2021-04-21 13:55:47 +0200

    update and delete project functionality

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async (dispatch) => {
   try {
@@ -20,4 +20,30 @@ export const getProjects = () => async (dispatch) => {
     type: GET_PROJECTS,
     payload: res.data,
   });
+};
+
+export const getProject = (id, history) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/project/${id}`);
+    dispatch({
+      type: GET_PROJECT,
+      payload: res.data,
+    });
+  } catch (error) {
+    history.push("/dashboard");
+  }
+};
+
+export const deleteProject = (id) => async (dispatch) => {
+  if (
+    window.confirm(
+      "Are you sure? This will delete the project and all the data related to it"
+    )
+  ) {
+    await axios.delete(`/api/project/${id}`);
+    dispatch({
+      type: DELETE_PROJECT,
+      payload: id,
+    });
+  }
 };
